36118e75f9498d34557c03a83a2f740e555e3434
tomfumb <thomaschristian@gmail.com>
2021-02-10 11:55:44 -0800

    Added OSM trail conversion

---
+++ main-data/osm-conversion/convert.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+IMAGE_NAME=bvnordic/osm-convert
+TMP_DIR=/data/osm-conversion/tmpdata
+TMP_JOINED=$TMP_DIR/joined.geojson
+
+cd `dirname $0`
+docker build -t $IMAGE_NAME .
+
+docker run --rm -w /data -v $PWD/..:/data $IMAGE_NAME ogr2ogr -sql "select trails.dog_friend as dog_friend, trails.lights as lights, trails.difficulty as difficulty, trail_names.trail_name as name from trails join 'trail_names.csv'.trail_names on trails.trail_id = trail_names.trail_id" $TMP_JOINED trails.shp
+
+docker run --rm -v $PWD/..:/data $IMAGE_NAME /source/ogr2osm/ogr2osm.py $TMP_JOINED -f -o $TMP_DIR/out.osm
+EXIT_CODE=$?
+
+docker run --rm -v $PWD/..:/data $IMAGE_NAME rm $TMP_JOINED
+
+
+exit $EXIT_CODE
