59ba69a2566f546be6a232090c43b049e46fa366
Tom Christian <thomaschristian@gmail.com>
2021-10-06 15:37:06 -0700

    BVN04 initial work on CI export

--- ci/export2.py
+++ ci/export2.py
@@ -10,28 +10,17 @@ from uuid import uuid4
 layout_name = environ.get("PDF_LAYOUT_NAME", "Print")
 filename = environ.get("PDF_OUTPUT_PATH", join(expanduser("~"), f"{uuid4()}.pdf"))
 
-print("1")
-
 QgsApplication.setPrefixPath("/usr/bin/qgis", True)
-print("2")
 qgs = QgsApplication([], False)
-print("3")
- 
-# Load providers
 qgs.initQgis()
-print("4")
-
 project = QgsProject.instance()
-print("5")
 project.read("./main.qgs")
-
-print("6")
 
 layoutmanager = project.layoutManager()
 layout_item = layoutmanager.layoutByName(layout_name)
 export = QgsLayoutExporter(layout_item)
-export.exportToPdf(filename, QgsLayoutExporter.PdfExportSettings())
- 
-# Finally, exitQgis() is called to remove the
-# provider and layer registries from memory
+settings = QgsLayoutExporter.PdfExportSettings()
+settings.dpi = 96
+export.exportToPdf(filename, settings)
+
 qgs.exitQgis()
--- ci/run.sh
+++ ci/run.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
 # Xvfb :1 -screen 0 1024x768x24 &
-xvfb-run -s '+extension GLX -screen 0 1024x768x24'
-export DISPLAY=:1.0
+# xvfb-run -s '+extension GLX -screen 0 1024x768x24'
+# export DISPLAY=:1.0
 
 cd `dirname $0`/..
 
 echo `pwd`
 
-echo "export 1"
-QGIS_DEBUG=9 qgis --nologo --project ./main.qgs --code ./ci/export.py
+# echo "export 1"
+# QGIS_DEBUG=9 qgis --nologo --project ./main.qgs --code ./ci/export.py
 
 echo "export 2"
-python3 ci/export2.py
+xvfb-run -s '+extension GLX -screen 0 1920x1080x24' python3 ci/export2.py
+
+# Xorg -noreset +extension GLX +extension RANDR +extension RENDER -logfile /xdummy.log -config /xorg.conf :1
