76b65df9a0b2609882bd5f7b1d5b6dcf6d9fb9b0
tomfumb <thomaschristian@gmail.com>
2021-10-13 14:32:47 -0700

    BVN04 update CI with SHA directory

--- scripts/create-dir.sh
+++ scripts/create-dir.sh
@@ -3,10 +3,7 @@
 UUID=$(uuidgen)
 UNIQUE_REF="${GITHUB_SHA:-$(uuidgen)}"
 EXPORT_REF=$(date -u '+%Y%m%d')-$UNIQUE_REF
-EXPORT_DIR=output/build/$EXPORT_REF
+export EXPORT_DIR=output/build/$EXPORT_REF
 
 cd $(dirname $0)/..
-
 mkdir -p $EXPORT_DIR
-
-OUTPUT_BASE=/export/$EXPORT_DIR scripts/generate-pdf.sh
---
+++ scripts/local-ci.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+. scripts/create-dir.sh
+OUTPUT_BASE=/export/$EXPORT_DIR scripts/generate.sh
