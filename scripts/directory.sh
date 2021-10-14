8e9fc79815835c8031054a5e924ae376e747af23
tomfumb <thomaschristian@gmail.com>
2021-10-13 21:13:40 -0700

    BVN04 upload attempt

--- scripts/directory.sh
+++ scripts/directory.sh
@@ -2,5 +2,5 @@
 
 UNIQUE_REF="${GITHUB_SHA:-$(uuidgen)}"
 EXPORT_REF=$(date -u '+%Y%m%d')-$UNIQUE_REF
-export LOCAL_DIR=output/build/$EXPORT_REF
-export CONTAINER_DIR=/export/$LOCAL_DIR
+export LOCAL_OUTPUT_DIR=output/build/$EXPORT_REF
+export CONTAINER_OUTPUT_DIR=/export/$LOCAL_DIR
