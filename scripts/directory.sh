f8eabad715d953e59c9ec7596683faa3282e8668
tomfumb <thomaschristian@gmail.com>
2021-10-13 21:39:38 -0700

    BVN04 debugging CI issues

--- scripts/directory.sh
+++ scripts/directory.sh
@@ -3,4 +3,4 @@
 UNIQUE_REF="${GITHUB_SHA:-$(uuidgen)}"
 EXPORT_REF=$(date -u '+%Y%m%d')-$UNIQUE_REF
 export LOCAL_OUTPUT_DIR=output/build/$EXPORT_REF
-export CONTAINER_OUTPUT_DIR=/export/$LOCAL_DIR
+export CONTAINER_OUTPUT_DIR=/export/$LOCAL_OUTPUT_DIR
