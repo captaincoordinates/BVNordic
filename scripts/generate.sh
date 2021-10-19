8e9ac33f826f40717931e7e00e1b257b411e401d
tomfumb <thomaschristian@gmail.com>
2021-10-18 21:37:39 -0700

    BVN04 attempting to fix path issues

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -3,4 +3,4 @@
 pushd $(dirname $0)/..
 
 docker build -t exporter output
-docker run --rm -v $PWD:/export -e OUTPUT_BASE exporter /export/output/generate.sh
+docker run --rm -v $PWD:/export -e OUTPUT_BASE=/export/$OUTPUT_BASE exporter /export/output/generate.sh
--- scripts/test.sh
+++ scripts/test.sh
@@ -5,5 +5,5 @@ pushd $(dirname $0)/..
 
 GITHUB_SHA=$(uuidgen) . scripts/directory.sh
 mkdir -p $LOCAL_OUTPUT_DIR
-OUTPUT_BASE=$CONTAINER_OUTPUT_DIR scripts/generate.sh
+OUTPUT_BASE=$LOCAL_OUTPUT_DIR scripts/generate.sh
 UPLOAD_FOLDER_NAME=$UNIQUE_DIR scripts/upload.sh
