5198eb6898d32b913e923cad1f33a4b0f41df1d0
tomfumb <thomaschristian@gmail.com>
2021-10-18 20:57:11 -0700

    BVN04 ...

--- scripts/directory.sh
+++ scripts/directory.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo "GITHUB_SHA: "$GITHUB_SHA
+echo "uuidgen: "$(uuidgen)
+echo "substitution: ""${GITHUB_SHA:-$(uuidgen)}"
+TMP="${GITHUB_SHA:-$(uuidgen)}"
+echo "TMP: "$TMP
+TMP2="_$TMP"
+echo "TMP2: "$TMP2
+
 export UNIQUE_DIR="${GITHUB_SHA:-$(uuidgen)}"
 export LOCAL_OUTPUT_DIR=output/build/$UNIQUE_DIR
 export CONTAINER_OUTPUT_DIR=/export/$LOCAL_OUTPUT_DIR
