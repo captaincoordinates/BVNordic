11f0919a4acc28ec15a5b55413db980d493c8d20
tomfumb <thomaschristian@gmail.com>
2021-10-20 11:01:46 -0700

    BVN04 DRY

--- output/generate.py
+++ output/generate.py
@@ -65,11 +65,12 @@ for layout in layout_manager.layouts():
     )
 
     if layout_name in thumbnails_by_layout:
+        size = thumbnails_by_layout[layout_name]["size"]
         thumbnail = Image.open(join(output_base, f"{layout_name}.png"))
         thumbnail.thumbnail(
             (
-                thumbnails_by_layout[layout_name]["size"],
-                thumbnails_by_layout[layout_name]["size"],
+                size,
+                size,
             )
         )
         thumbnail.save(join(output_base, f"{layout_name}-thumbnail.png"))
