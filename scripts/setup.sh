f77c6babb15fe80b440303b4c1e73dbf9d69a8bb
tomfumb <thomaschristian@gmail.com>
2021-10-31 14:43:21 -0700

    BVN14 debug PR workflow

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,4 +3,5 @@
 pip install -r requirements-dev.txt
 pip install -r cicd/compare/requirements-pr.txt
 pip install -r cicd/compare/requirements-detect.txt
+pip install -r cicd/upload/requirements.txt
 pre-commit install
