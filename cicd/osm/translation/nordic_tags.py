8902edcf13bfb050c75da0bf72d2f9763844bec9
tomfumb <thomaschristian@gmail.com>
2021-11-19 10:22:10 -0800

    BVN13 removed closed ways from routes, deleting single-node ways

--- cicd/osm/translation/nordic_tags.py
+++ cicd/osm/translation/nordic_tags.py
@@ -3,20 +3,23 @@ Translations for Nordic Centre trails.
 Dog-friendly trails are treated as one-way. These trails are only one-way for dogs but the OSM tags do not appear to support this nuance.
 """
 from os import environ
+from typing import Final  # type: ignore
 
 from ogr2osm import TranslationBase
+
+PISTE_PREFIX: Final = "piste:"
+BVNORDIC_PREFIX: Final = "bvnordic:"
 
 
 class NordicTags(TranslationBase):
     def filter_tags(self, tags):
         if not tags:
             return
-        piste_prefix = "piste:"
-        bvnordic_prefix = "bvnordic:"
         return {
-            "name": tags.get("name", ""),
-            f"{piste_prefix}type": "nordic",
-            f"{piste_prefix}difficulty": [
+            f"{BVNORDIC_PREFIX}trail_name": tags.get("trail_name", ""),
+            "name": tags.get("segment_name", ""),
+            f"{PISTE_PREFIX}type": "nordic",
+            f"{PISTE_PREFIX}difficulty": [
                 value
                 for key, value in {
                     (0, 1): "easy",
@@ -25,11 +28,12 @@ class NordicTags(TranslationBase):
                 }.items()
                 if int(tags.get("difficulty", 0)) in key
             ][0],
-            f"{piste_prefix}oneway": {0: "no", 1: "yes"}[int(tags.get("dog_friend", 0))],
+            f"{PISTE_PREFIX}oneway": {0: "no", 1: "yes"}[int(tags.get("dog_friend", 0))],
             "dog": {0: "no", 1: "yes"}[int(tags.get("dog_friend", 0))],
             "lit": {0: "no", 1: "yes"}[int(tags.get("lights", 0))],
-            f"{piste_prefix}grooming": "classic;skating",
+            f"{PISTE_PREFIX}grooming": "classic;skating",
             "network": "BV Nordic Centre",
-            f"{bvnordic_prefix}readme": "Created by Bulkley Valley Nordic Centre GIS. Please contact osm@bvnordic.ca before making any changes.",
-            f"{bvnordic_prefix}commit-sha": f"{environ['GITHUB_SHA']}",
+            f"{BVNORDIC_PREFIX}closed": {"0": "no", "1": "yes"}[str(tags.get("closed"))],
+            f"{BVNORDIC_PREFIX}readme": "Created by Bulkley Valley Nordic Centre GIS. Please contact osm@bvnordic.ca before making any changes.",
+            f"{BVNORDIC_PREFIX}commit-sha": f"{environ['GITHUB_SHA']}",
         }
