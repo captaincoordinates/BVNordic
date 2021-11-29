c6d44e3764a1955eebce004f960410d3df2824b7
tomfumb <thomaschristian@gmail.com>
2021-11-29 12:58:20 -0800

    BVN32 fix PR issue and update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,6 +5,7 @@ pip install -r cicd/compare/requirements-pr.txt
 pip install -r cicd/compare/requirements-detect.txt
 pip install -r cicd/upload/requirements.txt
 pip install -r cicd/osm/requirements.txt
+pip install -r cicd/imagery/requirements.txt
 pre-commit install
 echo "collecting satellite imagery for Stadium"
 cicd/imagery/scripts/update.sh
