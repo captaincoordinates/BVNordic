e8319f7c30cf176620b224a743e773a8f9a28410
tomfumb <thomaschristian@gmail.com>
2022-01-14 14:38:23 -0800

    BVN58 fixed python reference

--- cicd/imagery/scripts/update-stadium.sh
+++ cicd/imagery/scripts/update-stadium.sh
@@ -7,4 +7,4 @@ if [ "$CI" == "true" ]; then
     UPLOAD_IF_MISSING=1
 fi
 cicd/scripts/pull_or_build.sh repo=tomfumb image=bvnordic-imagery-updater:2 build_dir=cicd/imagery/docker context_dir=cicd/imagery upload_if_missing=$UPLOAD_IF_MISSING
-docker run --rm -v $PWD:/code -w /code tomfumb/bvnordic-imagery-updater:2 python -m cicd.imagery.stadium
+docker run --rm -v $PWD:/code -w /code tomfumb/bvnordic-imagery-updater:2 python3 -m cicd.imagery.stadium
